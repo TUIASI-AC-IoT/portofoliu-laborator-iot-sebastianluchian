e2f9bee3e498d270558f3009cb036ae5bea1ef31
Sebastian Luchian <90963555+sebastianluchian@users.noreply.github.com>
2025-04-28 19:17:43 +0300

    Add files via upload

---
+++ laborator4/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "7"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.7 - 2025-04-28 18:53:06.386545"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.7"
+#endif
